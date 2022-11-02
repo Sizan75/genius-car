592dea89a5d6a7216722f6c967fed5a9a639489d
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-03 01:53:38 +0600

    user created by email and pass

--- src/context/AuthProvider/AuthProvider.js
+++ src/context/AuthProvider/AuthProvider.js
@@ -1,7 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../../Firebase/firebase.config';
-import getAuth, { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
-const AuthContext=createContext()
+import  { createUserWithEmailAndPassword,getAuth, onAuthStateChanged } from 'firebase/auth'
+export  const AuthContext=createContext()
 const auth= getAuth(app)
 const AuthProvider =  ({children}) => {
     const [user,setUser]=useState(null)
