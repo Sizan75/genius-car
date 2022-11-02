6597abbb2e81c7250a0d42a2b13ee5171c88ff4b
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-02 12:19:17 +0600

    service card data added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
 <p className='text-2xl font-bold text-orange-600'>Services</p>
 const Services = () => {
     const [services, setServices]= useState([])
@@ -9,13 +10,17 @@ const Services = () => {
     },[])
     return (
         <div>
-            <div className='text-center'>
+            <div className='text-center mb-4'>
             <p className='text-2xl font-bold text-orange-600'>Services</p>
             <h2 className='text-5xl font-semibold'>Our Services Area</h2>
             <p className=''>the majority have suffered alteration in some form, by injected humour, or randomised <br/> words which don't look even slightly believable. </p>
         </div>
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {
+                services.map(service => <ServiceCard key={service._id}
+                service={service}
+                ></ServiceCard>)
+            }
         </div>
         </div>
 
