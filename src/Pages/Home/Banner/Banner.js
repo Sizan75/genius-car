47d390bb6ba1337a856b4b082edcff01264c2a01
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-02 10:03:27 +0600

    banner array created

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,34 +6,50 @@ import banner4 from '../../../assets/images/banner/4.jpg'
 import banner5 from '../../../assets/images/banner/5.jpg'
 import banner6 from '../../../assets/images/banner/6.jpg'
 import './Banner.css'
+import BannerItem from './BannerItem';
 const Banner = () => {
+    const bannerData = [
+        {
+            image: banner1,
+            prev: 6,
+            id: 1,
+            next: 2
+        },
+        {
+            image: banner2,
+            prev: 1,
+            id: 2,
+            next: 3
+        },
+        {
+            image: banner3,
+            prev: 2,
+            id: 3,
+            next: 4
+        },
+        {
+            image: banner4,
+            prev: 3,
+            id: 4,
+            next: 5
+        },
+        {
+            image: banner5,
+            prev: 4,
+            id: 5,
+            next: 6
+        },
+        {
+            image: banner6,
+            prev: 5,
+            id: 6,
+            next: 1
+        }
+    ]
     return (
         <div>
  <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <div className='carousel-img'>
-        <img src={banner1} alt='' className="w-full" /> </div>
-    <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4  ">
-    <h1 className='text-white font-bold text-5xl'>
-        Affordable <br/>
-        Price For Car <br/>
-        Service
-    </h1>
-     </div>
-    <div className="absolute flex w-2/5 justify-end transform -translate-y-1/2 left-24 top-1/2  ">
-    <p className='text-xl text-white'>
-    There are many variations of passages of  available, but the majority have suffered alteration in some form
-    </p>
-     </div>
-    <div className="absolute flex w-2/5 justify-start transform -translate-y-1/2 left-24 top-3/4  ">
-    <button className="btn btn-warning mr-5">Warning</button>
-    <button className="btn btn-outline btn-warning">Warning</button>
-     </div>
-    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-      <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
+  <BannerItem image={banner1}></BannerItem>
   <div id="slide2" className="carousel-item relative w-full">
     <img src={banner2} alt='' className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
