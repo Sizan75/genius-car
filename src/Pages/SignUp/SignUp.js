5bd4b8f08a20fa27feb0ac5b0c15e0d7047c8823
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-02 23:18:44 +0600

    sing up structure done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import login from '../../assets/images/login/login.svg'
-const Login = () => {
-    const handleLogin = (event ) =>{
+import signup from '../../assets/images/login/login.svg'
+const SignUp = () => {
+    const handleSignUp = (event ) =>{
         event.preventDefault();
     }
     return (
         <div className="hero w-full my-20">
         <div className="hero-content grid gap-20 md:grid-cols-2  flex-col lg:flex-row">
           <div className="text-center lg:text-left">
-       
-            <img className='w-3/4' src={login} alt="" />
+           
+            <img className='w-3/4' src={signup} alt="" />
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm py-20 shadow-2xl bg-base-100">
-            <h1 className="text-5xl font-bold text-center">Login</h1>
-            <form onSubmit={handleLogin} className="card-body">
+            <h1 className="text-5xl font-bold text-center">Sign</h1>
+            <form onSubmit={handleSignUp} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name='name' placeholder="Your Name" className="input input-bordered"  />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input name='email' type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
                 <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
-                <label className="label">
-                  <a href=" " className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
+                
               </div>
               <div className="form-control mt-6">
-                <button type='submit' className="btn btn-primary">Login</button>
+                <button type='submit' className="btn btn-primary">Sign Up</button>
               </div>
             </form>
-            <p className='text-center'>New to Genius Car? <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
+            <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to="/login">Log In</Link></p>
           </div>
         </div>
       </div>
     );
 };
 
-export default Login;
+export default SignUp;
