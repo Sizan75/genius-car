23fd7617b0d1ec1e5eb7be94aecc3eb42f640bf6
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-02 11:43:09 +0600

    service json added

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+<p className='text-2xl font-bold text-orange-600'>Services</p>
+const Services = () => {
+    return (
+        <div>
+            <p className='text-2xl font-bold text-orange-600'>Services</p>
+        </div>
+    );
+};
+
+export default Services;
