4a0483c3eeca49bfdeec5d047aaa7c1e4cf41ea6
Sizan75 <sakiuzzaman26@gmail.com>
2022-11-02 11:57:18 +0600

    Services component done

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 <p className='text-2xl font-bold text-orange-600'>Services</p>
 const Services = () => {
+    const [services, setServices]= useState([])
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res=>res.json())
+        .then(data=>setServices(data))
+    },[])
     return (
         <div>
+            <div className='text-center'>
             <p className='text-2xl font-bold text-orange-600'>Services</p>
+            <h2 className='text-5xl font-semibold'>Our Services Area</h2>
+            <p className=''>the majority have suffered alteration in some form, by injected humour, or randomised <br/> words which don't look even slightly believable. </p>
         </div>
+        <div>
+            
+        </div>
+        </div>
+
     );
 };
 
